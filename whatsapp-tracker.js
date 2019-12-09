278a8995fc913bb402a3cf04d29c71035bd0eb47
Gündoğdu Yakıcı <dodis@live.com>
2019-12-09 14:00:23 +0300

    Update whatsapp.tracker.js
    Whatsapp Web information retrieval class name change.

--- whatsapp-tracker.js
+++ whatsapp-tracker.js
@@ -2,7 +2,7 @@ setInterval(function() {
 	var dt = new Date();
 	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 	try {
-		var lastSeen = document.getElementsByClassName("O90ur")[0].innerText;
+		var lastSeen = document.getElementsByClassName("i1XSV")[0].innerText;
 		console.log(time + ' ' + lastSeen);
 	}
 	catch(err) {

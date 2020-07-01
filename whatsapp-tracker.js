2e9b08656a09a6404af637b09bfe9ac0cc058336
Gündoğdu Yakıcı <dodis@live.com>
2020-07-01 14:02:24 +0300

    Update Class Name

--- whatsapp-tracker.js
+++ whatsapp-tracker.js
@@ -2,7 +2,7 @@ setInterval(function() {
 	var dt = new Date();
 	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 	try {
-		var lastSeen = document.getElementsByClassName("O90ur _3FXB1")[0].innerText;
+		var lastSeen = document.getElementsByClassName("_2ruUq _3xjAz")[0].innerText;
 		console.log(time + ' ' + lastSeen);
 	}
 	catch(err) {

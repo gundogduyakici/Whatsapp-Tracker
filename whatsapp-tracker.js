ce697825674f0c308c4d628bc8c06a2470f30edb
Gündoğdu Yakıcı <dodis@live.com>
2021-06-16 13:44:31 +0300

    Error Message Fixed
    Aborted error mError message solution when there is no text in the class.essage when null value is returned.

--- whatsapp-tracker.js
+++ whatsapp-tracker.js
@@ -2,8 +2,10 @@ setInterval(function() {
 	var dt = new Date();
 	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 	try {
-		var lastSeen = document.getElementsByClassName("YmixP fKfSX")[0].innerText;
-		console.log(time + ' ' + lastSeen);
+		if(document.getElementsByClassName("YmixP fKfSX")[0]) {
+			var lastSeen = document.getElementsByClassName("YmixP fKfSX")[0].innerText;
+			console.log(time + ' ' + lastSeen);
+		}		
 	}
 	catch(err) {
 		console.log(err);

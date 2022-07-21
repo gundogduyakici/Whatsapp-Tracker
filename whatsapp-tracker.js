e46588a211a1671db0aaf4bfffb5af3f5c6ce296
Gündoğdu Yakıcı <dodis@live.com>
2022-07-21 14:49:33 +0200

    Update whatsapp-tracker.js
    Class name has been changed.

--- whatsapp-tracker.js
+++ whatsapp-tracker.js
@@ -2,7 +2,7 @@ setInterval(function() {
 	var dt = new Date();
 	var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
 	try {
-		if(document.getElementsByClassName("zzgSd _3e6xi")[0]) {
+		if(document.getElementsByClassName("_2YPr_ i0jNr")[0]) {
 			var lastSeen = document.getElementsByClassName("zzgSd _3e6xi")[0].innerText;
 			console.log(time + ' ' + lastSeen);
 		}		
